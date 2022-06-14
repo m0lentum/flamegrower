17c9b2d11f7e4026a3b994f66f05648534aa80a6
Mikael Myyrä <mikael.myyrae@gmail.com>
2022-06-15 00:52:06 +0300

    nix: Go back to Niv, update sources

---
+++ shell.nix
@@ -0,0 +1,38 @@
+let
+  sources = import ./nix/sources.nix;
+  rust-overlay = import sources.rust-overlay;
+  pkgs = import sources.nixpkgs { overlays = [ rust-overlay ]; };
+
+  rust = pkgs.rust-bin.stable."1.61.0".default;
+in
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.niv
+    # rust and profiling
+    rust
+    pkgs.cargo-flamegraph
+    pkgs.lld
+    pkgs.llvmPackages.bintools
+    pkgs.tracy
+    pkgs.renderdoc
+    # asset authoring
+    pkgs.tiled
+    pkgs.jq
+    pkgs.entr
+    pkgs.just
+    # wgpu C dependencies
+    pkgs.pkgconfig
+    pkgs.xorg.libX11
+  ];
+  # make C libraries available
+  LD_LIBRARY_PATH = with pkgs.xorg; with pkgs.lib.strings;
+    concatStrings (intersperse ":" [
+      "${libX11}/lib"
+      "${libXcursor}/lib"
+      "${libXxf86vm}/lib"
+      "${libXi}/lib"
+      "${libXrandr}/lib"
+      "${pkgs.vulkan-loader}/lib"
+      "${pkgs.stdenv.cc.cc.lib}/lib64"
+    ]);
+}
