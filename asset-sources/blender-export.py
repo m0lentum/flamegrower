07d4bb802520dac497757a40bede6aac589f1e53
Mikael Myyrä <mikael.myyrae@gmail.com>
2022-08-16 14:06:17 +0300

    Add blender export scripts + player walk animation

---
+++ asset-sources/blender-export.py
@@ -0,0 +1,38 @@
+"""Script for automatically exporting Blender models
+from the command line with standard settings used in Flamegrower."""
+
+import bpy
+import sys
+from bpy.app.handlers import persistent
+
+@persistent
+def export(*_):
+    try:
+        target_file = sys.argv[sys.argv.index("--") + 1]
+    except Exception:
+        print("Give the target file after a '--' on the command line")
+        return
+
+    bpy.ops.export_scene.gltf(
+        filepath=target_file,
+        check_existing=False,
+        export_format="GLB",
+        # export visible animations with vertex colors
+        use_visible=True,
+        export_colors=True,
+        export_skins=True,
+        export_animations=True,
+        export_morph=True,
+        optimize_animation_size=True,
+        export_yup=True,
+        # everything else we don't need
+        export_image_format="NONE",
+        export_texcoords=False,
+        export_normals=False,
+        export_tangents=False,
+        export_morph_normal=False,
+        export_morph_tangent=False,
+        export_materials="NONE",
+    )
+
+bpy.app.handlers.load_post.append(export)
--- justfile
+++ justfile
@@ -2,6 +2,11 @@
 run:
   cargo run
 
+# export all assets
+export-all:
+  just export-scenes
+  just export-models
+
 # whenever a scene in asset-sources changes, export it
 watch-scenes:
   ls asset-sources/scenes/*.json | entr just export-scene /_
@@ -9,10 +14,25 @@ watch-scenes:
 # export all scenes
 export-scenes:
   for scene in asset-sources/scenes/*.json; do \
-    just export-scene $scene; \
+    just export-scene "$scene"; \
   done
 
 # export a single scene given its file path
 export-scene scene:
   jq -f asset-sources/tiled/export.jq {{scene}} \
     > assets/scenes/$(basename {{scene}})
+
+# whenever a model in asset-sources changes, export it
+watch-models:
+  ls asset-sources/models/*.blend | entr just export-model /_
+
+# export all models
+export-models:
+  for model in asset-sources/models/*.blend; do \
+    just export-model "$model"; \
+  done
+
+# export a single model given its file path
+export-model model:
+  blender --background --python ./asset-sources/blender-export.py \
+    "{{model}}" -- "./assets/models/$(basename {{model}} .blend)"
